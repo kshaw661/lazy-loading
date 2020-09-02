6b3dfce49f5bd24dd2821d734eae18c187ce3cb9
Kunal <risk.always@gmail.com>
2020-09-02 11:50:50 +0530

    lazy Loading with Two Compoenent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lazy-loading';
+  title = 'lazy-comp';
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private cfr: ComponentFactoryResolver
+  ) {}
+
+  async getLazy1() {
+    this.viewContainerRef.clear();
+    console.log('Lazy100')
+    const { Lazy1Component } = await import('../app/components/lazy1/lazy1.component');
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy1Component)
+    );
+  }
+
+  async getLazy2() {
+    this.viewContainerRef.clear();
+    console.log('Lazy200')
+    const { Lazy2Component } = await import('../app/components/lazy2/lazy2.component');
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy2Component)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Lazy1Component } from './components/lazy1/lazy1.component';
+import { Lazy2Component } from './components/lazy2/lazy2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lazy1Component,
+    Lazy2Component
   ],
   imports: [
     BrowserModule
